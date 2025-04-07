0bbca52cf2eff4ec31aef3fc8a2deaa7df9aa638
Dylan Maupa <114060288+dylanmaupa@users.noreply.github.com>
2025-04-07 14:44:01 +0200

    deleted weight

--- project.py
+++ project.py
@@ -2,9 +2,8 @@ name = "Dylan"
 age = 25
 
 height = 5.9
-weight = 160
 
-print(f"Name: {name}, Age: {age}, Height: {height}, Weight: {weight}")
+print(f"Name: {name}, Age: {age}, Height: {height}")
 
 
 
